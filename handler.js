7d2e342df513ac739f30f462180eebd508c9cbfa
MoOmEeN <moomeen@gmail.com>
2017-03-13 02:24:51 +0100

    disable connection pooling

--- handler.js
+++ handler.js
@@ -80,7 +80,8 @@ exports.myHandler = function(event, context, callback) {
                       headers: {
                         'User-Agent': USER_AGENTS[parseInt(Math.random() * 10)]
                       },
-                      timeout: 10000
+                      timeout: 10000,
+                      pool: false
                     };
                     rp(options)
                         .then(function (body) {
